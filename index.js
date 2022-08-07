842fbfb706fc9237a11c80958fb4ebe15399e758
Eric Hsiao <ehsiao789@gmail.com>
2022-08-07 07:09:06 -0700

    Test commit with new pages and JavaScript

---
+++ index.js
@@ -0,0 +1,33 @@
+/* Set the current tab to be a darker color and have a larger weight */
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+if (page === "index.html") {
+    $("#about").css("color", "#077725");
+    $("#about").css("font-weight", "550");
+}
+else if (page === "skills.html") {
+    $("#skills").css("color", "#077725");
+    $("#skills").css("font-weight", "550");
+}
+else if (page === "calisthenics.html") {
+    $("#calisthenics").css("color", "#077725");
+    $("#calisthenics").css("font-weight", "550");
+}
+else if (page === "photos.html") {
+    $("#photos").css("color", "#077725");
+    $("#photos").css("font-weight", "550");
+}
+
+/* Remove the controls of a video unless hovered over */
+
+$(".vid").removeAttr("controls");
+
+$(".vid").hover(function(event) {
+    if(event.type === "mouseenter") {
+        $(this).attr("controls", "");
+    } else if(event.type === "mouseleave") {
+        $(this).removeAttr("controls");
+    }
+});
